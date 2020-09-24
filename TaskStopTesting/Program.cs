c56fe46c7135f295239e2a2f025f45df67f173fd
Roman <megamega4@yandex.ru>
2020-09-24 22:22:17 +0600

    Добавьте файлы проекта.

---
+++ TaskStopTesting/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskStopTesting
+{
+    class Program
+    {
+        private static CancellationTokenSource Canceller { get; set; }
+
+        static void DoWork()
+        {
+            for (int i = 0; i < 10000; i++)
+                Console.WriteLine("value" + i);
+
+            Console.WriteLine("Task finished!");
+        }
+
+        static void Main(string[] args)
+        {
+            Canceller = new CancellationTokenSource();
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    using (Canceller.Token.Register(Thread.CurrentThread.Abort))
+                    {
+                        DoWork();
+                    }
+                }
+                catch (ThreadAbortException)
+                {
+                    return;
+                }
+            }, Canceller.Token);
+
+            Canceller.CancelAfter(1000);
+
+            try
+            {
+                task.Wait(Canceller.Token);
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("is Completed: {0}", task.IsCompleted);
+            Console.WriteLine("is Canceled: {0}", task.IsCanceled);
+            Console.WriteLine("is Faulted: {0}", task.IsFaulted);
+            Console.WriteLine();
+            Console.WriteLine("is CanBeCanceled: {0}", Canceller.Token.CanBeCanceled);
+            Console.WriteLine("is CancellationRequested: {0}", Canceller.Token.IsCancellationRequested);
+
+            Console.ReadLine();
+        }
+    }
+}
